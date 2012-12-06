20aee8c3d7a18df0d1781cc111550f30fd9cfeb9
Tim Heap <tim@ionata.com.au>
2012-12-06 15:43:47 +1100

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='django-chainable-manager',
-    version="0.2.0",
+    version="0.3.0",
     description='Chainable methods on Model managers',
     author='Tim Heap',
     author_email='heap.tim@gmail.com',
